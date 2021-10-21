52fbf9d3eef66d6415c3d5d5d5c83f142e868975
piducancore:/# <hola@piducan.dev>
2021-10-21 19:50:02 -0300

    add theme-ui

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,10 @@
+import base from "@theme-ui/preset-base"
+import { merge } from "theme-ui"
+
+const theme = merge(base, {
+  fonts: {
+    body: "Montserrat, sans-serif",
+  },
+})
+
+export default theme
