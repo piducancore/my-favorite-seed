6287a9e238858e746e488aaad9a42ceffb80ebab
piducancore:/# <hola@piducan.dev>
2021-10-22 12:33:52 -0300

    use gatsby colors

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,6 +5,10 @@ import "@fontsource/saira/700.css"
 import "@fontsource/saira/400.css"
 
 const theme = merge(base, {
+  colors: {
+    primary: "#6d2f9c",
+    secondary: "#ffae1e",
+  },
   fontSizes: [12, 14, 16, 22, 24, 32, 48, 64, 96],
   fonts: {
     heading: `'Saira', sans-serif`,
